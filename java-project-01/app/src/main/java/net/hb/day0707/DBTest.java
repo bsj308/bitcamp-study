6b377527cd1dceb294ea4f2da837e5b2623df19d
bsj308 <bsj308@naver.com>
2021-07-07 20:29:54 +0900

    20210707 국비수업

---
+++ java-project-01/app/src/main/java/net/hb/day0707/DBTest.java
@@ -0,0 +1,16 @@
+package net.hb.day0707;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBTest {
+
+  public static void main(String[] args) {
+    Connection CN = null; //DB서버연결정보
+    Statement ST = null;
+    ResultSet RS = null;
+
+
+  }//end
+}//Class END
